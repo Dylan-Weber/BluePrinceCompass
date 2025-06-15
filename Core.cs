23db04c8a4f0400aa48673de61745e7a5cbe9392
Dylan Weber <34782089+Dylan-Weber@users.noreply.github.com>
2025-06-15 15:52:37 -0400

    Initialize Project

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(BluePrinceCompass.Core), "Blue Prince Compass Mod", "1.0.0", "ComplexSimple")]
+[assembly: MelonGame("Dogubomb", "BLUE PRINCE")]
+
+namespace BluePrinceCompass
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
